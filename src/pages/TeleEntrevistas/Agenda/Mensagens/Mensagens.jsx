e47696c852e034d0d8d9cf36555771e45f33cca0
Rodrigo Onesko Dias <rodrigo_onesko@hotmail.com>
2022-12-16 19:26:02 -0300

    update

--- src/pages/TeleEntrevistas/Agenda/Mensagens/Mensagens.jsx
+++ src/pages/TeleEntrevistas/Agenda/Mensagens/Mensagens.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Axios from 'axios'
 import moment from "moment";
 import Sidebar from "../../../../components/Sidebar/Sidebar";
@@ -11,11 +11,15 @@ const Mensagens = () => {
     const [horarios, setHorarios] = useState([])
     const [propostas, setPropostas] = useState([])
     const [dataMensagem, setDataMensagem] = useState('')
+    const [mensagens, setMensagens] = useState([])
 
-    const gerarMensagens = () => {
+    const gerarMensagens = async () => {
         try {
-            buscarPropostas()
-            buscarHorarios(dataMensagem)
+            const result = await Axios.get(`${process.env.REACT_APP_API_KEY}/entrevistas/mensagens/${dataMensagem}`)
+
+            console.log(result);
+
+            setMensagens(result.data.msgs)
 
         } catch (error) {
             console.log(error);
@@ -76,10 +80,6 @@ const Mensagens = () => {
         }
     }
 
-    useEffect(() => {
-
-    })
-
     return (
         <>
             <Sidebar></Sidebar>
@@ -95,47 +95,21 @@ const Mensagens = () => {
                     </div>
                     <div className="mensagens">
                         {
-                            propostas.map(e => {
-
-                                let qtd = 0
-                                let associados = new Map()
-
-                                propostas.forEach(element => {
-                                    if (element.proposta == e.proposta) {
-                                        qtd++
-                                        associados.set(`${element.nome}`, `${element.sexo}`)
-                                    }
-                                })
-
-                                if (qtd >= 2) {
-                                    associados.forEach((item, key) => {
-                                        console.log(item, key);
-                                    })
-                                }
-
-                                let sexo
-                                if (e.sexo === 'M') {
-                                    sexo = 'Prezado Sr.'
-                                }
-                                if (e.sexo === 'F') {
-                                    sexo = 'Prezada Sra.'
-                                }
-
-                                let diaSemana = ajustarDiaSemana(moment(dataMensagem).format('dddd'))
-
-                                if (e.tipoAssociado == 'Titular') {
-                                    return (
-                                        <div key={e._id} className='mensagem'>
-                                            <p>{sexo} {e.nome} proposta: {e.proposta}</p>
-                                            <p>Somos da equipe de adesão da operadora de saúde Amil e para concluirmos a sua contratação ao Plano de Saúde precisamos confirmar alguns dados para que a contratação seja concluída.</p>
-                                            <p>Precisamos entrar em contato através do número 85999809731. Temos disponíveis os horários para dia *{moment(dataMensagem).format('DD/MM/YYYY')}* {diaSemana} as 09:00 - 09:20 - 09:40 - 10:00 - 10:20 - 10:40 - 11:00 - 11:20 - 11:40 - 12:00 - 12:20 - 12:40 - 13:00 - 13:20 - 13:40 - 14:00 - 14:20 - 14:40 - 15:00 - 15:20 - 15:40 - 16:00 - 16:20 - 16:40 - 17:00 - 17:20 - 17:40 - 18:00 - 18:20 - 18:40 - 19:00 - 19:20 - Qual melhor horário?</p>
-                                            <p>Informamos que vamos ligar dos números 11 42404975 ou 42403554, pedimos tirar do spam para evitar bloqueio da ligação.</p>
-                                            <p>Desde já agradecemos.</p>
-                                        </div>
-                                    )
-                                }
-
-
+                            mensagens.map(e=>{
+                                return(
+                                    <div className="mensagem">
+                                        {e.saudacao}
+                                        <br />
+                                        {e.parte1} {e.parte2} {e.parte3}
+                                        <br />
+                                        {e.parte4} {e.parte5} {e.parte6}
+                                        <br />
+                                        <br />
+                                        {e.parte7}
+                                        <br />
+                                        <span>Tipo Contrato: {e.tipoContrato}</span>
+                                    </div>
+                                )
                             })
                         }
                     </div>
