e33cb20184db2bb630983a2cdcff0f9e969b786d
Rodrigo Onesko Dias <rodrigo_onesko@hotmail.com>
2023-07-10 17:30:08 -0300

    update

--- src/pages/Admin/ControlePoliticas/Modals/ModalAdicionarPolitica.jsx
+++ src/pages/Admin/ControlePoliticas/Modals/ModalAdicionarPolitica.jsx
@@ -1,14 +1,48 @@
 import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@mui/material"
 import { useState } from "react"
 import { FaPlus } from 'react-icons/fa'
+import PdfUploader from "../PdfUploader"
 
 const ModalAdicionarPolitica = () => {
 
     const [open, setOpen] = useState(false)
+    const [file, setFile] = useState()
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const handleUpload = async () => {
+        console.log(file);
+    }
 
     return (
         <>
-            <Button variant="contained"><FaPlus /></Button>
+            <Button variant="contained" onClick={handleClickOpen} ><FaPlus /></Button>
+
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">
+                    Adicionar ou atualizar nova política
+                </DialogTitle>
+                <DialogContent>
+                    <PdfUploader setFile={setFile} file={file} />
+                </DialogContent>
+                <DialogActions>
+                    <Button variant="contained" color='inherit' onClick={handleClose}>Fechar</Button>
+                    <Button variant="contained" onClick={handleUpload} autoFocus>
+                        Upload
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </>
     )
 }
---
+++ src/pages/Admin/ControlePoliticas/PdfUploader.jsx
@@ -0,0 +1,71 @@
+import { useRef, useState } from "react";
+import { Box, Paper, Typography } from "@mui/material";
+import { FaRegFilePdf } from 'react-icons/fa'
+
+const PdfUploader = ({ setFile, file }) => {
+
+    const fileInputRef = useRef(null)
+    const [onDrag, setOnDrag] = useState(false)
+
+    const handleDrop = (e) => {
+        e.preventDefault()
+        setFile(e.dataTransfer.files[0]);
+        setOnDrag(false)
+    }
+
+    const handleDragOver = (e) => {
+        e.preventDefault()
+        setOnDrag(true)
+    }
+
+    const handleFileChange = (e) => {
+        setFile(e.target.files[0])
+    }
+
+    const handleClick = () => {
+        fileInputRef.current.click()
+    }
+
+    return (
+        <Box
+            component={Paper}
+            elevation={2}
+            onDrop={handleDrop}
+            onDragOver={handleDragOver}
+            onDragLeave={() => {
+                setOnDrag(false)
+            }}
+            p={10}
+            onClick={handleClick}
+        >
+            {
+                onDrag ? (
+                    <Typography>
+                        Solte o pdf
+                    </Typography>
+                ) : null
+            }
+            {
+                file ? (
+                    <object data={URL.createObjectURL(file)} type="application/pdf" height={500} >
+                        PDF
+                    </object>
+                ) : (
+                    <Typography>
+                        Arraste e solte o pdf aqui <FaRegFilePdf />
+                    </Typography>
+                )
+            }
+
+            <input
+                type="file"
+                ref={fileInputRef}
+                onChange={handleFileChange}
+                multiple
+                style={{ display: 'none' }}
+            />
+        </Box >
+    )
+}
+
+export default PdfUploader
