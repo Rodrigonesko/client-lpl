4383e6b60e53a76a592296a8d4e0ecb396f4b837
Leonardo <leonardoarrudalonque@outlook.com>
2023-10-06 13:40:57 -0300

    Update Férias

--- src/_services/ferias.js
+++ src/_services/ferias.js
@@ -1,2 +1,5 @@
 import { ApiCall } from "./api";
 
+export const UpdateFerias = async (data) => {
+    return await new ApiCall('/vacation/update').put(data)
+}
---
+++ src/pages/Ferias/Solicitacao/Modais/ModalEditarFerias.jsx
@@ -0,0 +1,77 @@
+import { Alert, Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, Snackbar, TextField } from "@mui/material";
+import { useState } from "react";
+import { UpdateFerias } from "../../../../_services/ferias";
+
+const ModalEditarFerias = ({ setFlushHook, id, trocaData }) => {
+
+    const [open, setOpen] = useState(false);
+    const [openSnack, setOpenSnack] = useState(false)
+    const [data, setData] = useState(trocaData)
+    const [textoSnack, setTextoSnack] = useState('')
+    const [severitySnack, setSeveritySnack] = useState('')
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const handleSave = async () => {
+        try {
+            const result = await UpdateFerias({
+                _id: id,
+                data
+            })
+            console.log(result)
+            setFlushHook(true)
+            setOpenSnack(true)
+            setSeveritySnack('success')
+            setTextoSnack('Texto atualizado com sucesso!')
+            setOpen(false)
+            return
+
+        } catch (error) {
+            console.log(error.response.data.msg);
+            setTextoSnack('Erro ao solicitar férias.' + error.response.data.msg)
+            setSeveritySnack('error')
+            setOpenSnack(true)
+        }
+    }
+
+    return (
+        <>
+            <Button onClick={handleClickOpen} variant='contained' color="success" size="small">Editar</Button>
+
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">
+                    {"Editar Férias Existente"}
+                </DialogTitle>
+                <DialogContent sx={{ width: '500px' }}>
+                    <Box display='flex' flexDirection='column' minHeight='150px' justifyContent='space-around'>
+                        <TextField type="date" label='Altere a data de Férias selecionada' focused size="small" value={data} onChange={e => setData(e.target.value)} />
+                    </Box>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>Fechar</Button>
+                    <Button onClick={handleSave} variant="contained" color="success">
+                        Atualizar
+                    </Button>
+                </DialogActions>
+            </Dialog>
+            <Snackbar open={openSnack} autoHideDuration={6000} onClose={() => setOpenSnack(false)}>
+                <Alert variant="filled" onClose={() => setOpenSnack(false)} severity={severitySnack} sx={{ width: '100%' }}>
+                    {textoSnack}
+                </Alert>
+            </Snackbar>
+        </>
+    )
+}
+
+export default ModalEditarFerias;
