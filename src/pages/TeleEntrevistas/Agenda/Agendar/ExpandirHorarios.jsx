b56f17791dd35e99be8078be0d54246f907e402b
Rodrigo Onesko Dias <51219687+Rodrigonesko@users.noreply.github.com>
2023-10-09 19:13:59 -0300

    ajustando horarios

---
+++ src/pages/TeleEntrevistas/Agenda/Agendar/ExpandirHorarios.jsx
@@ -0,0 +1,41 @@
+import { ExpandLess, ExpandMore } from "@mui/icons-material";
+import { Box, Collapse, IconButton, Paper, Tooltip, Typography } from "@mui/material";
+import { useState } from "react";
+
+const ExpandirHorarios = ({ analistasDisponiveis, dia }) => {
+
+    const [open, setOpen] = useState(false);
+
+    const handleClick = () => {
+        setOpen(!open);
+    };
+
+    return (
+        <>
+            <Tooltip title='Expandir Horários'>
+                <IconButton onClick={handleClick}>
+                    {open ? <ExpandLess /> : <ExpandMore />}
+                </IconButton>
+            </Tooltip>
+
+            <Collapse in={open} timeout="auto" unmountOnExit>
+                {
+                    analistasDisponiveis[dia].map(item => {
+                        return (
+                            <Box component={Paper} p={1} mb={1}>
+                                <strong>{item.horario + ': '}</strong>
+                                {
+                                    item.analistas.map(analista => {
+                                        return <span style={{color: 'gray'}}>{analista} - </span>
+                                    })
+                                }
+                            </Box>
+                        )
+                    })
+                }
+            </Collapse>
+        </>
+    )
+}
+
+export default ExpandirHorarios
