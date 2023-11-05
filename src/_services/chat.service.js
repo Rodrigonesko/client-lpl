6ad0a367bbdb80a66856ee46839fdc0b239ee639
Rodrigo Onesko Dias <51219687+Rodrigonesko@users.noreply.github.com>
2023-11-05 19:16:31 -0300

    Atulizando modulo de chat

--- src/_services/chat.service.js
+++ src/_services/chat.service.js
@@ -1,5 +1,7 @@
 import { ApiCall } from "./api";
+import { getCookie } from "react-use-cookie";
 const URL_API = process.env.REACT_APP_CHAT_SERVICE
+const token = getCookie('token')
 
 export const sendMessageInterno = async ({ receptor, mensagem, chatId, resposta }) => {
     return await new ApiCall('/sendMessage', URL_API).post({ receptor, mensagem, chatId, resposta })
@@ -19,4 +21,24 @@ export const createGroupChat = async ({ participantes, nome }) => {
 
 export const seeInternalMessage = async ({ chatId }) => {
     return await new ApiCall('/seeMessage', URL_API).patch({ chatId })
+}
+
+export const uploadArquivosChat = async (data) => {
+    return await new ApiCall('/upload', URL_API, token, { headers: { "Content-Type": `multipart/form-data; boundary=${data._boundary}` } }).post(data)
+}
+
+export const createPrivateChat = async ({ receptor }) => {
+    return await new ApiCall('/createPrivateChat', URL_API).post({ receptor })
+}
+
+export const leaveChat = async ({ chatId }) => {
+    return await new ApiCall('/leaveChat', URL_API).patch({ chatId })
+}
+
+export const addPartipant = async ({ chatId, participante }) => {
+    return await new ApiCall('/addParticipant', URL_API).patch({ chatId, participante })
+}
+
+export const deleteParticipant = async ({ chatId, participante }) => {
+    return await new ApiCall('/leaveChat', URL_API).patch({ chatId, participante })
 }
