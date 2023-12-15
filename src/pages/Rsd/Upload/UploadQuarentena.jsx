5cc3247a2a2c955159ae1955766abb185cab3e0c
Leonardo <leonardoarrudalonque@outlook.com>
2023-12-15 14:28:00 -0300

    Update RSD

--- src/pages/Rsd/Upload/UploadQuarentena.jsx
+++ src/pages/Rsd/Upload/UploadQuarentena.jsx
@@ -1,16 +1,14 @@
 import React, { useState } from "react";
 import Sidebar from "../../../components/Sidebar/Sidebar";
-import Modal from 'react-modal'
 import { subirPedidos, uploadAltaFrequencia } from "../../../_services/rsd.service";
-
-Modal.setAppElement('#root')
+import { Button, CircularProgress, Dialog, DialogActions, DialogTitle } from "@mui/material";
 
 const UploadQuarentena = () => {
 
     const [file, setFile] = useState()
     const [status, setStatus] = useState('')
     const [pedidos, setPedidos] = useState([])
-    const [modal, setModal] = useState(false)
+    const [open, setOpen] = useState(false)
 
     const send = async e => {
         e.preventDefault()
@@ -22,8 +20,6 @@ const UploadQuarentena = () => {
             formData.append('file', file, file.name)
 
             setStatus('Enviando...')
-
-            //const result = await Axios.post(`${process.env.REACT_APP_API_KEY}/rsd/uploadQuarentena`, formData, { headers: { "Content-Type": `multipart/form-data; boundary=${formData._boundary}` }, withCredentials: true })
 
             const result = await uploadAltaFrequencia(formData)
 
@@ -42,19 +38,21 @@ const UploadQuarentena = () => {
 
     const subir = async () => {
         try {
-            //const result = await Axios.post(`${process.env.REACT_APP_API_KEY}/rsd/subir`, { pedidos }, { withCredentials: true })
-
             const result = await subirPedidos({
                 pedidos
             })
 
             setStatus(`Foram adicionados ${result.pedidos.length} pedidos`)
-            setModal(true)
+            setOpen(true)
 
         } catch (error) {
             console.log(error);
             setStatus('Algo deu errado')
         }
+    }
+
+    const handleClose = () => {
+        setOpen(false)
     }
 
     return (
@@ -66,67 +64,64 @@ const UploadQuarentena = () => {
                             <p>{status}</p>
                             {
                                 status !== 'Enviando...' ? (
-                                    <button onClick={subir}>Subir</button>
-                                ) : null
+                                    <Button type='submit' onClick={subir} variant='contained' size='large'>Subir</Button>
+                                ) : (
+                                    <CircularProgress />
+                                )
                             }
                         </div>
                     ) : null}
                     <div className="upload-container">
                         <form action="" method="post" encType="multipart/form-data">
                             <div className="title">
-                                <h2>Upload da Fila Quarentena</h2>
+                                <h2>Upload da Fila Alta Frequência</h2>
                             </div>
                             <div>
                                 <label htmlFor="file-rn">Arquivo: </label>
                                 <input type="file" name="file-rn" id="file-rn" onChange={e => setFile(e.target.files[0])} />
                             </div>
                             <div className="container-btns">
-                                <button className="btn" onClick={send}>Enviar</button>
+                                <Button type='submit' onClick={send} variant='contained' size='large' >Enviar</Button>
                             </div>
                         </form>
                     </div>
                     <div className="pedidos-tabela">
-                        <table className="table-novos-pedidos">
-                            <thead>
-                                <tr>
-                                    <th>Pedido</th>
-                                    <th>Nome</th>
-                                    <th>Protocolo</th>
-                                    <th>Valor apresentado</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {
-                                    pedidos.map(e => {
-                                        return (
-                                            <tr>
-                                                <td>{e[0]}</td>
-                                                <td>{e[8]}</td>
-                                                <td>{e[11]}'</td>
-                                                <td>{e[9]}</td>
-                                            </tr>
-                                        )
-                                    })
-                                }
-                            </tbody>
-                        </table>
+                        {
+                            status !== 'Enviando...' ? (
+                                <table className="table-novos-pedidos">
+                                    <thead>
+                                        <tr>
+                                            <th>Pedido</th>
+                                            <th>Nome</th>
+                                            <th>Protocolo</th>
+                                            <th>Valor apresentado</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {
+                                            pedidos.map(e => {
+                                                return (
+                                                    <tr>
+                                                        <td>{e[0]}</td>
+                                                        <td>{e[8]}</td>
+                                                        <td>{e[11]}'</td>
+                                                        <td>{e[9]}</td>
+                                                    </tr>
+                                                )
+                                            })
+                                        }
+                                    </tbody>
+                                </table>
+                            ) : (
+                                <CircularProgress />
+                            )}
                     </div>
-                    <Modal
-                        isOpen={modal}
-                        onRequestClose={() => { setModal(false) }}
-                        contentLabel="Exemplo"
-                        overlayClassName='modal-overlay'
-                        className='modal-content'>
-                        <div className="title">
-                            <h2>{status}</h2>
-                        </div>
-                        <div className="btns-modal">
-                            <button onClick={() => {
-                                setModal(false)
-                                window.location.reload()
-                            }}>Fechar</button>
-                        </div>
-                    </Modal>
+                    <Dialog open={open} onClose={handleClose} aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description">
+                        <DialogTitle id="alert-dialog-title">{status}</DialogTitle>
+                        <DialogActions>
+                            <Button onClick={handleClose}>Fechar</Button>
+                        </DialogActions>
+                    </Dialog>
                 </section>
             </Sidebar>
         </>
