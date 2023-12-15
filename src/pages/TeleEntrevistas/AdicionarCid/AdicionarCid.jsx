bf072c4500dc9fe83245ff4fd63064cbae03fbcd
Leonardo <leonardoarrudalonque@outlook.com>
2023-12-15 11:53:33 -0300

    Update tele

--- src/pages/TeleEntrevistas/AdicionarCid/AdicionarCid.jsx
+++ src/pages/TeleEntrevistas/AdicionarCid/AdicionarCid.jsx
@@ -1,76 +1,92 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Sidebar from "../../../components/Sidebar/Sidebar";
-import Modal from 'react-modal'
 import { adicionaCid } from "../../../_services/teleEntrevista.service";
-
-Modal.setAppElement('#root')
+import { Alert, Box, Button, Container, Snackbar, TextField } from "@mui/material";
 
 const AdicionarCid = () => {
 
-    const [modalIsOpen, setModalIsOpen] = useState(false)
+    const [openSnack, setOpenSnack] = useState(false)
+    const [textoSnack, setTextoSnack] = useState('Insira o Cid!')
+    const [severitySnack, setSeveritySnack] = useState('')
+    const [flushHook, setFlushHook] = useState(false)
 
     const [cid, setCid] = useState('')
     const [descricaoCid, setDescricaoCid] = useState('')
 
-
-    const openModal = () => {
-        setModalIsOpen(true)
-    }
-    const closeModal = () => {
-        setModalIsOpen(false)
-    }
+    const handleCloseSnack = () => {
+        setOpenSnack(false);
+    };
 
     const adicionarCid = async (e) => {
         try {
-
             e.preventDefault()
 
-            await adicionaCid({ cid, descricao: descricaoCid })
+            if ((cid.length < 1) || (descricaoCid.length < 1)) {
+                setOpenSnack(true)
+                setTextoSnack('Cid ou Descrição precisa ser adicionada!')
+                setSeveritySnack('warning')
+                return
+            }
 
-            openModal()
+            const resultado = await adicionaCid({ cid, descricao: descricaoCid })
+
+            if (resultado.msg === 'Cid ja registrado!') {
+                setOpenSnack(true);
+                setSeveritySnack('warning');
+                setTextoSnack(`Cid ja registrado!`);
+                return
+            }
+
+            console.log(resultado)
+            setOpenSnack(true)
+            setTextoSnack('Cid adicionada com sucesso!')
+            setSeveritySnack('success')
+            setFlushHook(true)
+            setCid('')
+            setDescricaoCid('')
+            return
 
         } catch (error) {
             console.log(error);
+            setTextoSnack('Erro ao criar Cid.' + error.response.data.msg)
+            setSeveritySnack('error')
+            setOpenSnack(true)
         }
     }
+
+    useEffect(() => {
+        setFlushHook(false)
+    }, [flushHook])
 
     return (
         <>
             <Sidebar>
-                <section>
-                    <div>
-                        <div className="title">
-                            <h3>Adicionar Cid</h3>
-                        </div>
-                        <form>
-                            <div>
-                                <label htmlFor="">Cid: </label>
-                                <input type="text" name="" id="" onKeyUp={e => setCid(e.target.value)} />
-                            </div>
-                            <div>
-                                <label htmlFor="">Descrição Cid: </label>
-                                <input type="text" onKeyUp={e => setDescricaoCid(e.target.value)} />
-                            </div>
-                            <div>
-                                <button onClick={adicionarCid}>Adicionar</button>
-                            </div>
-                        </form>
+                <Container maxWidth>
+                    <div className="title">
+                        <h2>Adicionar Cid</h2>
                     </div>
-                    <Modal
-                        isOpen={modalIsOpen}
-                        onRequestClose={closeModal}
-                        contentLabel="Exemplo"
-                        overlayClassName='modal-overlay'
-                        className='modal-content'>
-                        <div className="title">
-                            <h2>Cid Adicionado com sucesso!</h2>
-                        </div>
-                        <button onClick={() => {
-                            closeModal()
-                            window.location.reload()
-                        }}>Ok</button>
-                    </Modal>
-                </section>
+                    <Box>
+                        <Box>
+                            <br />
+                            <Box>
+                                <TextField type='text' variant='outlined' label='Cid' size='small' value={cid} onChange={e => setCid(e.target.value)} />
+                            </Box>
+                            <br />
+                            <Box>
+                                <TextField type='text' variant='outlined' label='Descrição Cid' size='small' value={descricaoCid} onChange={e => setDescricaoCid(e.target.value)} />
+                            </Box>
+                            <br />
+                            <Box>
+                                <Button type='submit' onClick={adicionarCid} variant='contained' size='large' >Adicionar</Button>
+                            </Box>
+                        </Box>
+                    </Box>
+                    <Snackbar open={openSnack} autoHideDuration={6000} onClose={handleCloseSnack}>
+                        <Alert variant="filled" onClose={handleCloseSnack} severity={severitySnack} sx={{ width: '100%' }}>
+                            {textoSnack}
+                        </Alert>
+                    </Snackbar>
+                </Container>
             </Sidebar>
         </>
     )
