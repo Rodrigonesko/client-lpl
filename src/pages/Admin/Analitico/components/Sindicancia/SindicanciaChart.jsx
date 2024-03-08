cc3e5e8b6f86c2d0d8bc4a836a0ea98fff309064
Leonardo <leonardoarrudalonque@outlook.com>
2024-03-08 11:18:45 -0300

    Update Leonardo

--- src/pages/Admin/Analitico/components/Sindicancia/SindicanciaChart.jsx
+++ src/pages/Admin/Analitico/components/Sindicancia/SindicanciaChart.jsx
@@ -1,7 +1,24 @@
-import { Box, Paper, Typography } from "@mui/material"
+import { Box, CircularProgress, Paper, Typography } from "@mui/material"
+import { useEffect, useState } from "react"
 import Chart from "react-apexcharts"
+import { getQuantidadeDemandasSindicancia } from "../../../../../_services/sindicancia.service"
 
 const SindicanciaChart = ({ mes }) => {
+
+    const [loading, setLoading] = useState(false)
+    const [grafico, setGrafico] = useState([])
+
+    useEffect(() => {
+        const fetch = () => {
+            setLoading(true)
+            const result = getQuantidadeDemandasSindicancia(mes)
+            console.log(result);
+            setGrafico(result)
+            setLoading(false)
+        }
+        fetch()
+    }, [])
+
     return (
         <Box
             component={Paper}
@@ -22,45 +39,49 @@ const SindicanciaChart = ({ mes }) => {
                     (Últimos 30 dias)
                 </Typography>
             </Box>
-                <Chart
-                    height={500}
-                    type="line"
-                    series={[{
-                        name: 'Demandas',
-                        data: [
-                            30, 40, 35, 50, 49, 60, 70
-                        ]
-                    }]}
-                    options={{
-                        chart: {
-                            type: 'line',
-                            zoom: {
-                                enabled: false
-                            }
-                        },
-                        dataLabels: {
-                            enabled: false
-                        },
-                        stroke: {
-                            curve: 'straight'
-                        },
-                        title: {
-                            text: 'Demandas Diárias',
-                            align: 'left'
-                        },
-                        grid: {
-                            row: {
-                                colors: ['#f3f3f3', 'transparent'],
-                                opacity: 0.5
-                            },
-                        },
-                        xaxis: {
-                            categories: [
-                                '01', '02', '03', '04', '05', '06', '07', 
-                            ],
-                        }
-                    }}
-                />
+            {
+                loading ? <CircularProgress size={'100px'} />
+                    : (
+                        <Chart
+                            height={500}
+                            type="line"
+                            series={[{
+                                name: 'Demandas',
+                                data: [
+                                    30, 40, 35, 50, 49, 60, 70
+                                ]
+                            }]}
+                            options={{
+                                chart: {
+                                    type: 'line',
+                                    zoom: {
+                                        enabled: false
+                                    }
+                                },
+                                dataLabels: {
+                                    enabled: false
+                                },
+                                stroke: {
+                                    curve: 'straight'
+                                },
+                                title: {
+                                    text: 'Demandas Diárias',
+                                    align: 'left'
+                                },
+                                grid: {
+                                    row: {
+                                        colors: ['#f3f3f3', 'transparent'],
+                                        opacity: 0.5
+                                    },
+                                },
+                                xaxis: {
+                                    categories: [
+                                        '01', '02', '03', '04', '05', '06', '07',
+                                    ],
+                                }
+                            }}
+                        />
+                    )}
         </Box>
     )
 }
