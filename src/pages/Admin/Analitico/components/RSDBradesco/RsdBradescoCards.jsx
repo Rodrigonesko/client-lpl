6d10ee01dd383154ce1b2171ddb5d12901fc03ce
Leonardo Lonque <leonardoarrudalonque@outlook.com>
2024-06-28 11:43:51 -0300

    Update Analitico Rsd Bradesco

--- src/pages/Admin/Analitico/components/RSDBradesco/RsdBradescoCards.jsx
+++ src/pages/Admin/Analitico/components/RSDBradesco/RsdBradescoCards.jsx
@@ -1,5 +1,7 @@
-import { AlignHorizontalLeft, AutorenewOutlined, Cancel, Done } from "@mui/icons-material"
+import { AlignHorizontalLeft, AutorenewOutlined, Cancel, Done, KeyboardDoubleArrowUpOutlined } from "@mui/icons-material"
 import CardDashboardRsdBradesco from "../../../../../components/Card/CardDashboardRsdBradesco"
+import { Box, Grid, Typography } from "@mui/material"
+import { grey } from "@mui/material/colors"
 
 const RsdBradescoCards = ({ data }) => {
 
@@ -8,7 +10,35 @@ const RsdBradescoCards = ({ data }) => {
             <CardDashboardRsdBradesco title='Concluídos Pacotes' value={data.concluidos} icon={<Done color="success" />} />
             <CardDashboardRsdBradesco title='A Iniciar Pacotes' value={data.aIniciar} icon={<Cancel color="error" />} />
             <CardDashboardRsdBradesco title='Em Andamento Pacotes' value={data.emAndamento} icon={<AutorenewOutlined color="primary" />} />
-            <CardDashboardRsdBradesco title='Total Pacotes' value={data.total} icon={<AlignHorizontalLeft color="warning" />} />
+            <CardDashboardRsdBradesco title='Agendados Pacotes' value={data.agendados} icon={<AlignHorizontalLeft color="warning" />} />
+            {/* <CardDashboardRsdBradesco title='Total Pacotes' value={data.total} icon={<AlignHorizontalLeft color="warning" />} /> */}
+            <Grid item xs={12} sm={6} lg={12}>
+                <Box
+                    p={2}
+                    display='flex'
+                    justifyContent={'space-between'}
+                    sx={{
+                        borderRadius: '10px',
+                        bgcolor: grey[100],
+                    }}
+                    maxWidth
+                >
+                    <Box>
+                        <Typography variant='body1' fontWeight={'bold'}>Total Pacotes</Typography>
+                        <Typography variant='h4' fontWeight={'bold'}>{data.total}</Typography>
+                    </Box>
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            p: 1
+                        }}
+                    >
+                        {<KeyboardDoubleArrowUpOutlined color="secondary" />}
+                    </Box>
+                </Box>
+            </Grid>
         </>
     )
 }
